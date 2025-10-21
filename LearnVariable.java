acb88543eb4cdcfe96496e17b706b38b6fabc7a0
rioalkaseno <rioalkaseno91@gmail.com>
2025-10-21 15:48:21 +0700

    Add Scanner and Input umur

--- LearnVariable.java
+++ LearnVariable.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class LearnVariable {
     public static void main(String[] args) {
         int umur;
         String nama;
+
+        Scanner newScan = new Scanner(System.in);
+
+        System.out.println("Masukan umur = ");
+        umur = newScan.nextInt();
+
+         System.out.println("Masukan nama = ");
+        nama = newScan.nextLine();
+
         
     }
 }
